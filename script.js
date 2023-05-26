701d5b3f9c45205cf5b67c0d234dffcf26a6738a
Hector Cardenas <card.hectorl@gmail.com>
2023-05-25 20:15:03 -0500

    binary tree practice

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ class Tree {
 
 	buildTree(arr) {
 		const sortedArray = mergeSort(arr);
+
 		let start = 0;
 		let end = sortedArray.length - 1;
 		//If there is no other nodes to left or right of array left / right gets set to null.
@@ -66,7 +67,6 @@ class Tree {
 	}
 
 	delete(value, root = this.root) {
-	
 		//Base Case
 		if (root == null) {
 			return root;
@@ -80,19 +80,17 @@ class Tree {
 		}
 		//If it is not less or greater it is equal soo...
 		//Since the value is the same as root, Then this node will be deleted.
-		else
-		{	
+		else {
 			//Node with only one child or no children.
 			if (root.left == null) {
 				return root.right;
-			} else if (root.right == null){
+			} else if (root.right == null) {
 				return root.left;
 			}
 			//Node with two children: Get the inorder successor (Next biggest number in the right sub tree, AKA The smallest in the right subtree.)
-				root.data = this.minValue(root.right);
+			root.data = this.minValue(root.right);
 			//This deletes the inorder successor
-				root.right = this.delete(root.data, root.right);
-		
+			root.right = this.delete(root.data, root.right);
 		}
 
 		return root;
@@ -104,17 +102,17 @@ class Tree {
 			return root;
 		}
 		if (value < root.data) {
-			root = this.find(value,root.left)
+			root = this.find(value, root.left);
 		}
 		if (value > root.data) {
-			root = this.find(value,root.right)
+			root = this.find(value, root.right);
 		}
 		return root;
 	}
-//Iteration Level Order Func
-	levelOrder( func) {
-		let root = this.root
-		let queue = []
+	//Iteration Level Order Func
+	levelOrder(func) {
+		let root = this.root;
+		let queue = [];
 		queue.push(root);
 		// let values = [];
 
@@ -126,120 +124,235 @@ class Tree {
 				queue.push(queue[0].right);
 			}
 
-			func(queue[0])
+			func(queue[0]);
 
 			queue.shift();
 		}
 	}
-//Recursive Level Order func	
-	RecursivelevelOrder(func, root = this.root, queue = [null]) {
-		
+	//Recursive Level Order func
+	RecursivelevelOrder(func, root = this.root, array = [null]) {
 		//If no function is given
-		if(func == undefined) {
-			this.RecursivelevelOrder(printValue,root,queue)
+		if (func == undefined) {
+			this.RecursivelevelOrder(printValue, root, array);
 		}
 
-		if (queue.length == 0) {
+		if (array.length == 0) {
 			return;
 		}
 
-		if (queue[0] == null) {
-			
-			queue.shift();
-			queue.push(root);
-		}
-		
-		if (queue[0].left != null) {
-			queue.push(queue[0].left);
-		}
-		if (queue[0].right != null) {
-			queue.push(queue[0].right);
+		if (array[0] == null) {
+			array.shift();
+			array.push(root);
 		}
 
-		func(queue[0])
+		if (array[0].left != null) {
+			array.push(array[0].left);
+		}
+		if (array[0].right != null) {
+			array.push(array[0].right);
+		}
 
-		queue.shift();
+		func(array[0]);
 
-		this.RecursivelevelOrder(func,root = queue[0], queue = queue);
-		
+		array.shift();
+
+		this.RecursivelevelOrder(func, (root = array[0]), (array = array));
 	}
-	
+
 	//Inorder traveses from smallest node to biggest. Left to right of tree
-	inOrder(func, root = this.root, queue = [null]){
-		
-		if(func == undefined) {
-			//TODO Change this to return values in a inOrder form.
-			this.RecursivelevelOrder(printValue,root,queue)
+	inOrder(func, root = this.root, array = []) {
+		//Returns an array of the node values inorder if no function is given.
+		if (func === undefined) {
+			var blah = function () {
+				/* empty because ... */
+			};
+			func = blah;
+			this.inOrder(blah, root, array);
+			console.log(array);
+			return array;
 		}
-		
-		if (queue.length == 0) {
+
+		if (root.left != null) {
+			this.inOrder(func, root.left, array);
+
+			func(root);
+			array.push(root.data);
+			if (root.right != null) {
+				this.inOrder(func, root.right, array);
+			}
+		}
+
+		if (root.left == null) {
+			func(root);
+			array.push(root.data);
+			//Checks root.right to make sure the rest of the tree is visited.
+			if (root.right != null) {
+				this.inOrder(func, root.right, array);
+			}
 			return;
 		}
-		
-		if (queue[0] == null) {
-			
-			queue.shift();
-			queue.push(root);
-		}
-		
-		if (queue[queue.length-1].left != null) {
-			queue.push(queue[queue.length-1].left)
-			this.inOrder(func,root = queue[queue.length-1].left, queue = queue);
-			
-			if (root == null) {
-				return
-			}
-			
-			console.log('this is the smallest node');
-			console.log(queue[queue.length-1]);
-			
-		}
-		
+		//
 	}
 	//Preorder traveses in root -> left tree -> right tree
-	preOrder(func, root = this.root, queue = [null]){
+	preOrder(func, root = this.root, array = []) {
 		if (root == null) {
-			//TODO Change this to return array of values in pre order.
-			this.RecursivelevelOrder(printValue,root,queue)
+			this.RecursivelevelOrder(printValue, root, array);
 		}
-		func(root);
-		
+		if (func != undefined) {
+			func(root);
+			array.push(root);
+			// console.log(array);
+		} else {
+			var blah = function () {
+				/* empty because ... */
+			};
+			func = blah;
+			this.preOrder(func, root, array);
+			return array;
+		}
+
 		if (root.left != null) {
-			this.preOrder(func, root.left, queue = [1])		
-			
+			this.preOrder(func, root.left, array);
 		}
 		if (root.right != null) {
-			this.preOrder(func, root.right, queue = [1])
+			this.preOrder(func, root.right, array);
 		}
-		
+
 		if ((root.left && root.right) == null) {
 			return;
 		}
-
 	}
 
 	//Postorder traverses starting from bottom level of left tree to top of left tree -> bottom level right tree to top of right tree, then ending with root node.
-	postOrder(func, root = this.root){
+	postOrder(func, root = this.root, array = []) {
 		if (func == undefined) {
-			//TODO print array of vals in post order.
-			console.log('print values in post order')
+			var blah = function () {
+				/* empty because ... */
+			};
+			func = blah;
+			this.postOrder(func, root, array);
+			console.log(array);
+			return array;
 		}
-		if(root.left != null) {
-			this.postOrder(func, root.left);
+		if (root.left != null) {
+			this.postOrder(func, root.left, array);
 		}
-		if(root.right != null) {
-			this.postOrder(func, root.right);
+		if (root.right != null) {
+			this.postOrder(func, root.right, array);
 		}
 		if (root.left == null && root.right == null) {
 			func(root);
+			array.push(root);
 			return;
 		}
 		func(root);
+		array.push(root);
 	}
+	
+	height(value,root = this.root, height = 0, level = 0) {
+		if (value == root.data) {
+			if ((root.left == null) && (root.right == null)) {
+				//level = 1 return height as 0;
+				return height;
+			}
+
+			if ((root.left != null) || (root.right != null)) {
+				if (height == level) {
+					height++;
+					level++;
+				}
+				if (root.left != null) {
+					height = this.height(root.left.data,root.left,height,level)
+				}
+				if (root.right != null) {
+					if (level == 1) {
+						height = 1;
+					}
+					height =  this.height(root.right.data,root.right,height,level)
+				}
+			}
+			
+		} else if (value < root.data) {
+			let leftNode = root.left;
+			return this.height(value, leftNode);
+		} else if (value > root.data) {
+			let rightNode = root.right;
+			return this.height(value, rightNode);
+		}
+
+		return height;
+	
+
+	}
+
+	depth(nodeVal,root = this.root, depthCount = 0){
+		if ((depthCount == 0) && (nodeVal == root.data)) {
+			return depthCount;
+		}
+		if (nodeVal < root.data) {
+			depthCount++;
+			return this.depth(nodeVal, root.left, depthCount);
+		}
+		if (nodeVal > root.data) {
+			depthCount++;
+			return this.depth(nodeVal, root.right, depthCount)
+		}
+
+		if (nodeVal == root.data) {
+			return depthCount;
+		}
+
+	}
+
+	isBalanced(nodeTree = this.root){
+		let leftTreeHeight = this.height(nodeTree.left.data);	
+		let rightTreeHeight = this.height(nodeTree.right.data);
+		let differenceLeft = leftTreeHeight - rightTreeHeight;
+		let differenceRight = rightTreeHeight - leftTreeHeight;
+		
+		if ((differenceLeft === 1) || (differenceRight === 0))  {
+			console.log('tree is balanced');
+			return true;
+		} else {
+			console.log('tree is not balanced');
+			return false;
+
+		}
+
+	}
+
+	rebalance() {
+		let balancedArray = this.inOrder();
+		this.root = this.buildTree(balancedArray);
+		return this.root;
+	}
+}
+
+function removeDuplicates(array) {
+	//remove duplicated from sorted array
+	let i = 0;
+	let j = 1;
+	while (true) {
+		if (j == array.length) {
+			break;
+		}
+		if (array[i] == array[j]) {
+			array.splice(i,1);
+			continue;
+		} else if (array[i] != array[j]) {
+			i++;
+			j++;
+			continue;
+		}
+	}
+	console.log('duplicates removed: ' + array);
+	return array;
 
 }
 
+//Seperate Functions...
 function mergeSort(array) {
+
 	if (array.length < 2) {
 		return array;
 	}
@@ -271,8 +384,11 @@ function mergeArrays(left, right) {
 			break;
 		}
 	}
+
+
 	return sortedArray;
 }
+
 //Code from TOP to print out the binary search tree.
 const prettyPrint = (node, prefix = "", isLeft = true) => {
 	if (node.right !== null) {
@@ -288,23 +404,38 @@ function printValue(node) {
 	console.log(node.data);
 }
 
+function printNodeValues(node) {
+	console.log('printing.... ' + node.data);
+}
+
 //Testing
-// let sortedBB = mergeSort([3, 1, 4, 9, 0, 5, 9, 5, 6, 1, 1, 1, 0, 0, 6]);
-// console.log(sortedBB);
+function runTestScript() {
+	let arrOne = [4, 1, 6, 85, 7, 8, 2, 3,10,9,12,55,0];
+	removeDuplicates(arrOne);
+	const binaryTree = new Tree(arrOne);
+	binaryTree.isBalanced();
+	binaryTree.levelOrder(printNodeValues);
+	binaryTree.preOrder(printNodeValues);
+	binaryTree.postOrder(printNodeValues);
+	binaryTree.inOrder()
+	binaryTree.insert(24);
+	binaryTree.insert(76);
+	binaryTree.insert(16);
+	binaryTree.insert(44);
+	binaryTree.insert(42);
+	binaryTree.insert(98);
+	binaryTree.insert(23);
+	binaryTree.isBalanced();
+	binaryTree.rebalance();
+	
+	binaryTree.levelOrder(printNodeValues);
+	binaryTree.postOrder(printNodeValues);
+	binaryTree.inOrder(printNodeValues)
+	binaryTree.preOrder(printNodeValues);
 
-let arrOne = [4, 1, 6, 5, 7, 8, 2, 3, 10, 12];
 
-const treeOne = new Tree(arrOne);
-// treeOne.delete(8);
-// treeOne.insert(9);
-// treeOne.delete(10);
-// treeOne.find(3);
-// treeOne.levelOrder(printValue);
-// treeOne.RecursivelevelOrder();
-// treeOne.inOrder(printValue);
-// treeOne.preOrder(printValue);
-treeOne.postOrder(printValue);
-prettyPrint(treeOne.root);
+	prettyPrint(binaryTree.root);
+}
 
-// console.log(treeOne.root);
-//Testing
+runTestScript();
+
